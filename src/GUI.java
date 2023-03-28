ff10ddfb8d2491ec18c881d331dcbb4bae7cf4f1
Gabbe16 <94972340+Gabbe16@users.noreply.github.com>
2023-03-28 15:11:01 +0200

    fixade menyn och en keybind + open/save WIP

--- src/GUI.java
+++ src/GUI.java
@@ -11,29 +11,37 @@ public class GUI {
         button1.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                String filename = "info.txt";
-                BufferedReader in = null;
-                try {
-                    in = new BufferedReader(new FileReader(filename));
-                } catch (FileNotFoundException ex) {
-                    textArea1.setText("");
-                    return;
-                }
-                String nextLine = null;
-                try {
-                    nextLine = in.readLine();
-                    while (nextLine != null) {
-                        textArea1.append(nextLine + "\n");
-                        nextLine = in.readLine();
-                    }
-                } catch (IOException ex) {
-                    textArea1.setText("");
-                }
+                open();
             }
         });
     }
 
+    private void open() {
+        String filename = "info.txt";
+        BufferedReader in = null;
+        try {
+            in = new BufferedReader(new FileReader(filename));
+        } catch (FileNotFoundException ex) {
+            textArea1.setText("");
+            return;
+        }
+        String nextLine = null;
+        try {
+            nextLine = in.readLine();
+            while (nextLine != null) {
+                textArea1.append(nextLine + "\n");
+                nextLine = in.readLine();
+            }
+        } catch (IOException ex) {
+            textArea1.setText("");
+        }
+    }
+
     public void save() {
+        Save();
+    }
+
+    private static void Save() {
         String filename = "test.txt";
         PrintWriter out = null;
         try {
@@ -57,34 +65,34 @@ public class GUI {
         menuBar = new JMenuBar();
 
         //Build the first menu.
-        menu = new JMenu("A Menu");
+        menu = new JMenu("Arkiv");
         menu.setMnemonic(KeyEvent.VK_A);
         menu.getAccessibleContext().setAccessibleDescription(
                 "The only menu in this program that has menu items");
         menuBar.add(menu);
 
         //a group of JMenuItems
-        menuItem = new JMenuItem("A text-only menu item",
+        menuItem = new JMenuItem("Open",
                 KeyEvent.VK_T);
         //menuItem.setMnemonic(KeyEvent.VK_T); //used constructor instead
         menuItem.setAccelerator(KeyStroke.getKeyStroke(
-                KeyEvent.VK_1, ActionEvent.ALT_MASK));
+                KeyEvent.VK_O, ActionEvent.CTRL_MASK));
         menuItem.getAccessibleContext().setAccessibleDescription(
                 "This doesn't really do anything");
         menuItem.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-
+                open();
             }
         });
         menu.add(menuItem);
 
-        menuItem = new JMenuItem("Both text and icon");
-        menuItem.setMnemonic(KeyEvent.VK_B);
+        menuItem = new JMenuItem("Spara");
+        menuItem.setMnemonic(KeyEvent.VK_O);
         menuItem.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-
+                save();
             }
         });
         menu.add(menuItem);
@@ -97,89 +105,6 @@ public class GUI {
 
             }
         });
-        menu.add(menuItem);
-
-        //a group of radio button menu items
-        menu.addSeparator();
-        ButtonGroup group = new ButtonGroup();
-
-        rbMenuItem = new JRadioButtonMenuItem("A radio button menu item");
-        rbMenuItem.setSelected(true);
-        rbMenuItem.setMnemonic(KeyEvent.VK_R);
-        group.add(rbMenuItem);
-        rbMenuItem.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-
-            }
-        });
-        menu.add(rbMenuItem);
-
-        rbMenuItem = new JRadioButtonMenuItem("Another one");
-        rbMenuItem.setMnemonic(KeyEvent.VK_O);
-        group.add(rbMenuItem);
-        rbMenuItem.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-
-            }
-        });
-        menu.add(rbMenuItem);
-
-        //a group of check box menu items
-        menu.addSeparator();
-        cbMenuItem = new JCheckBoxMenuItem("A check box menu item");
-        cbMenuItem.setMnemonic(KeyEvent.VK_C);
-        cbMenuItem.addItemListener(new ItemListener() {
-            @Override
-            public void itemStateChanged(ItemEvent e) {
-
-            }
-        });
-        menu.add(cbMenuItem);
-
-        cbMenuItem = new JCheckBoxMenuItem("Another one");
-        cbMenuItem.setMnemonic(KeyEvent.VK_H);
-        cbMenuItem.addItemListener(new ItemListener() {
-            @Override
-            public void itemStateChanged(ItemEvent e) {
-
-            }
-        });
-        menu.add(cbMenuItem);
-
-        //a submenu
-        menu.addSeparator();
-        submenu = new JMenu("A submenu");
-        submenu.setMnemonic(KeyEvent.VK_S);
-
-        menuItem = new JMenuItem("An item in the submenu");
-        menuItem.setAccelerator(KeyStroke.getKeyStroke(
-                KeyEvent.VK_2, ActionEvent.ALT_MASK));
-        menuItem.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-
-            }
-        });
-        submenu.add(menuItem);
-
-        menuItem = new JMenuItem("Another item");
-        menuItem.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-
-            }
-        });
-        submenu.add(menuItem);
-        menu.add(submenu);
-
-        //Build second menu in the menu bar.
-        menu = new JMenu("Another Menu");
-        menu.setMnemonic(KeyEvent.VK_N);
-        menu.getAccessibleContext().setAccessibleDescription(
-                "This menu does nothing");
-        menuBar.add(menu);
 
         return menuBar;
     }
